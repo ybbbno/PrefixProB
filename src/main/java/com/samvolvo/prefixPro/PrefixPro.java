a11cb09000a96de74fbd23ceb5f54290390a153e
mitsu-wq <bmitsu779@gmail.com>
2026-06-11 18:07:05 +0700

    Remove maven

--- src/main/java/com/samvolvo/prefixPro/PrefixPro.java
+++ src/main/java/com/samvolvo/prefixPro/PrefixPro.java
@@ -12,6 +12,7 @@ import com.samvolvo.prefixPro.managers.Metrics;
 import com.samvolvo.prefixPro.utils.Logger;
 import com.samvolvo.prefixPro.utils.Messages;
 import com.samvolvo.prefixPro.utils.UpdateChecker;
+import me.deadybbb.ybmj.PluginProvider;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.event.node.NodeAddEvent;
 import net.luckperms.api.event.node.NodeRemoveEvent;
@@ -19,9 +20,8 @@ import net.luckperms.api.event.user.UserDataRecalculateEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PrefixPro extends JavaPlugin {
+public final class PrefixPro extends PluginProvider {
     private LuckPerms luckPerms;
     private PrefixManager prefixManager;
     private AfkManager afkManager;
